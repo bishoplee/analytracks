2baa6285fed68b14b1ad157afa93d179500ae8f1
bishoplee <raph.davids@gmail.com>
2014-12-16 12:19:46 +0000

    Create analytracks.js

---
+++ analytracks.js
@@ -0,0 +1,8 @@
+//1 -- Meta inclusion
+    var __lc = {};
+    __lc.license = 3054792;
+    //(function() {
+        var lc = document.createElement('meta'); lc.name = 'author'; lc.content = 'OladipoOlaleye<raph.davids@gmail.com>';
+        var s = document.getElementsByTagName('meta')[3]; s.parentNode.insertBefore(lc, s);
+    //})();
+//Meta inclusion
